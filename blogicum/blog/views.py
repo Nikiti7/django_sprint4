f5072762afeb288ae60f33d8598c0f261958bd15
Nikolay Oleynik <nikolayoleynik7@mail.ru>
2025-04-02 21:49:41 +0300

    correction PEP8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -44,12 +44,15 @@ posts = [
     },
 ]
 
+
 def index(request):
     return render(request, 'index.html', {'posts': posts})
+
 
 def post_detail(request, id):
     post = next((p for p in posts if p['id'] == id), None)
     return render(request, 'detail.html', {'post': post})
+
 
 def category_posts(request, category_slug):
     return render(request, 'category.html', {'category_slug': category_slug})
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 
+
 def about(request):
-    """Статическая страница "О нас" """
     return render(request, 'about.html')
 
+
 def rules(request):
-    """Статическая страница "Правила" """
     return render(request, 'rules.html')
-
-
