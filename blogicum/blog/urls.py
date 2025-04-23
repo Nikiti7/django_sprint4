416db09fb9f3b0ed1f7ad3b88d82d2d5ae472c88
Nikolay Oleynik <nikolayoleynik7@mail.ru>
2025-04-23 19:11:09 +0300

    0.3.05

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
     path("category/<slug:category_slug>/",
-         views.category_posts,
-         name="category_posts"),
+        views.category_posts,
+        name="category_posts"),
     path("posts/create/", views.post_create, name="post_create"),
     path("posts/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path("posts/<int:post_id>/comment/",
-         views.add_comment,
-         name="add_comment"),
+        views.add_comment,
+        name="add_comment"),
     path(
         "posts/<int:post_id>/edit_comment/<int:comment_id>/",
         views.edit_comment,
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("", include("blog.urls", namespace='blog')),
     path("pages/", include("pages.urls")),
     path("auth/", include("django.contrib.auth.urls")),
     path("auth/", include("users.urls")),
